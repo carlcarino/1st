15005cc8ea4e01495dcf2fdc742b26f7a3ef9bd1
carlcarino <78774779+carlcarino@users.noreply.github.com>
2021-02-09 12:17:13 +0800

    Update and rename helloworld.php to GroupMembers.php

---
+++ GroupMembers.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Group Members</h1>
+
+<?php
+  echo "Members:";
+  echo "Carl Angelo Carino";
+  echo "Lawrence Balmores";
+  echo "Patricia Bernadette Anne Beroy";
+  echo "Hannah Jane Bacani";
+  echo "Genesis De Guzman";
+?>
+
+</body>
+</html>
